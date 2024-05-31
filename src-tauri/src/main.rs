b91f02cd80446cd48396da941e198a24bac5fecc
J. Simon Richard <jsimonrichard@gmail.com>
2024-05-31 10:43:55 -0400

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,16 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+async fn greet(name: &str) -> Result<String, String> {
+    return Ok(format!("Hello, {}! You've been greeted from Rust!", name));
+}
+
+fn main() {
+    // #[allow(clippy::implicit_return)] // This hides the implicit_return lint above
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
